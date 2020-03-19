dc1500dadf387e214dc8da4fd5de9357f09c0567
Susant Paudel <crupee.susant@gmail.com>
2020-03-19 14:15:30 +0545

    path changes

---
+++ app/Http/Controllers/Pages/PageController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Pages;
+
+use App\Http\Controllers\Controller;
+
+class PageController extends Controller
+{
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
