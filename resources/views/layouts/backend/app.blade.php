2e99d990018fd72c978a59a8bac47c701ff7fee4
Susant <gracysusant@gmail.com>
2020-03-31 19:45:49 +0545

    nothing just

--- resources/views/layouts/backend/app.blade.php
+++ resources/views/layouts/backend/app.blade.php
@@ -114,8 +114,8 @@
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            {{Auth::user()->userID}}
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
+
+                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"></span>
                                 <img class="img-profile rounded-circle"
                                     src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
                             </a>
