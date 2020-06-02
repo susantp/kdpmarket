df10bfb827f6e668b77dd79382acfd3a192cc468
sandeepSpark <srijanup@outlook.com>
2020-06-02 08:24:58 +0545

    cancel button working

--- resources/views/backend/company/bonusindex.blade.php
+++ resources/views/backend/company/bonusindex.blade.php
@@ -194,15 +194,16 @@ WHERE TIMESTAMPDIFF(SECOND,sr.bonus_at,"'.$bonus_date['bonus_at'].'") > 0 ORDER 
             // console.log(value);
 
              $('#bonusResultsTable tbody').append("<tr><td>"+(index+1)+"</td><td>"+value.bonus_at+"</td><td>"+value.userID+"</td><td><button type='button' class='btn btn-danger cancelButton'>Cancel</button></td></tr>");
+              $('.cancelButton').on('click',function(e){
+      e.preventDefault();
+      $('#bonusResultsTable tbody').html('');
+  });
          });
       }
     });
   });
 
-  $('.cancelButton').on('click',function(e){
-      e.preventDefault();
-      $('#bonusResultsTable tbody').html('');
-  });
+ 
 });
 
 </script>
